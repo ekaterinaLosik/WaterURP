bc705b23a3a300b95dc8dce0f1ca7e3d23a1efff
ekaterinaLosik <nor11324@gmail.com>
2020-09-07 13:25:11 +0200

    first commit
    test

---
+++ Assets/OceanShaderGraph/badWeather.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class badWeather : MonoBehaviour
+{
+    public Material waves;
+    public float waveHeight = 1;
+    public bool enableBadWeather = false;
+    public Toggle toggle;
+    public Color colorGWTop = new Color(0.5986117f, 0.6739377f, 0.7169812f,0);
+    public Color colorGWBot = new Color(0.4300463f, 0.5845828f, 0.6037736f,0);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        waves.SetFloat("Vector1_F8D9205E", waveHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (toggle.isOn)
+        {
+            waveHeight = 2.5f;
+            waves.SetFloat("Vector1_F8D9205E", waveHeight);
+            waves.SetFloat("Vector1_C8CD7BB7", 0.25f);
+            waves.SetColor("Color_31F2026D", Color.black);
+            waves.SetColor("Color_C8989E96", Color.black);
+
+        }
+         if (!toggle.isOn)
+        {
+            waveHeight = 1;
+            waves.SetFloat("Vector1_F8D9205E", waveHeight);
+            waves.SetFloat("Vector1_C8CD7BB7", 0.1f);
+            waves.SetColor("Color_31F2026D",  colorGWTop);
+            waves.SetColor("Color_C8989E96",  colorGWBot);
+
+        }
+        
+    }
+}
